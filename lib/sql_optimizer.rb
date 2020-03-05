cffd46fd9fb6848d855ecfccca519e104b8ffc08
Nikita Korchma <nkorchma.codica@gmail.com>
2020-03-05 22:02:22 +0200

    Add analyze

---
+++ lib/sql_optimizer.rb
@@ -0,0 +1,8 @@
+require 'sql_optimizer/version'
+require 'sql_optimizer/analyze'
+
+module SqlOptimizer
+
+  class Error < StandardError; end
+
+end
---
+++ lib/sql_optimizer/version.rb
@@ -0,0 +1,5 @@
+module SqlOptimizer
+
+  VERSION = '0.1.0'.freeze
+
+end
